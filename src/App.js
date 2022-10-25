311f2b80d2f699ac4eba7492bb3281478995bd03
adiagne <alia.diagne.1@ulaval.ca>
2022-10-25 12:59:09 -0400

    products list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import './App.css';
+import data from './data';
 
 function App() {
   return (
     <div className="App">
-    <a href="/">alizane</a>
+      <header>
+        <a href="/">alizane</a>
+      </header>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+        {
+          data.products.map(product => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name}></img>
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p><strong>${product.price}</strong></p>
+                <button>Add to Cart</button>
+              </div>
+            </div>
+          ))
+        }
+        </div>
+      </main>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,54 @@
+const data = {
+    products: [
+        {
+            name: 'Nike Slim Shirt',
+            slug: 'nike-slim-shirt',
+            category: 'Shirts',
+            image: '/images/nike_shirt.jpg',
+            price: '120',
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality shirt'
+        },
+        {
+            name: 'Nike Slim Pants',
+            slug: 'nike-slim-pants',
+            category: 'Shirts',
+            image: '/images/nike_pants.jpg',
+            price: '90',
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality pant'
+        },
+        {
+            name: 'Adidas Slim Shirt',
+            slug: 'adidas-slim-shirt',
+            category: 'Shirts',
+            image: '/images/adidas_shirt.jpg',
+            price: '120',
+            countInStock: 10,
+            brand: 'Adidas',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality shirt'
+        },
+        {
+            name: 'Adidas Slim Pants',
+            slug: 'adidas-slim-pants',
+            category: 'Shirts',
+            image: '/images/adidas_pants.jpg',
+            price: '100',
+            countInStock: 10,
+            brand: 'Adidas',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality shirt'
+        }
+    ]
+}
+
+export default data
